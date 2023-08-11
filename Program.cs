a50dd17f14313a33e04d1b6fc7287bc5eab20c2b
hp <hp@DESKTOP-8LSNP3N>
2023-08-11 08:58:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+
+                    arr[i, j] = i + j;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+
+                    Console.Write(arr[i, j] + " ");
+                Console.WriteLine();
+            } 
+            Console.ReadLine();
+
+        }
+    }
+}
